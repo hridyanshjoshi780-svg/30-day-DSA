a5f5c5db3902b6acb7e836e2c33d6642b0da1b92
Hridyansh joshi <hridyanshjoshi780@gmail.com>
2025-09-01 21:42:02 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -1,13 +1,12 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main (){
-    int a ;
-    int b ;
-    cout<<"enter a number : ";
-    cin>>a;
-    cout<<"enter a number : ";
-    cin>>b;
-    int c = a+b;
-    cout<<c<<" is the sum of "<<a<<" and "<<b<<endl;
+int main() {
+    int a, b;
+    cout << "Enter the first number: ";
+    cin >> a;
+    cout << "Enter the second number: ";
+    cin >> b;
+    int sum = a + b;
+    cout << "The sum of " << a << " and " << b << " is: " << sum << endl;
     return 0;
 }
